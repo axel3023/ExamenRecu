4ce4fe319fdd20de62c6cbdd2aa2e8a636af6e7d
axel3023 <133275903+axel3023@users.noreply.github.com>
2024-02-07 22:56:14 -0600

    Examen Recuperación

--- examenRecu/src/main/java/edu/utez/mx/examenRecu/model/dao/PersonaDao.java
+++
@@ -1,9 +0,0 @@
-package edu.utez.mx.examenRecu.model.dao;
-
-import edu.utez.mx.examenRecu.model.entity.PersonaBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaDao extends JpaRepository<PersonaBean,Integer> {
-}
